355ee51ad5a67d533363715ee0a653311cf35155
Asloric <43351133+Asloric@users.noreply.github.com>
2023-02-06 18:58:33 +0100

    Added bot detection feature
    Bot detection feature based on posts per hour. Those are automaticaly added to the bots.txt and can be blocked on next launch.
    Usage:
    -Select browser
    - Select scan (3)
    Browse fresh in order to reach at least 1h old posts (you can stop before, but it might produce false positives).
    Don't overdo it or some bots might get through.

--- main.py
+++ main.py
@@ -41,53 +41,157 @@ def askForReportAndBlock():
         print("\n\n\n\nYou must choose between 1 or 2")
         return askForReportAndBlock()
 
+def postTimeToHour(value, unit):
+    # return the time since post in hour
+    if unit == "d":
+        return int(value) * 24
+    if unit == "h":
+        return int(value)
+    if unit == "m":
+        return int(value) / 60
+
 chooseBrowser()
 
-driver.get("https://9gag.com/login")
-input("Log into 9gag. Press ENTER after login.")
+if scanAccounts:
+    driver.get("https://9gag.com/fresh")
 
-with open('bots.txt', 'r') as file:
-    for bot in file:
-        bot = bot.strip()
-        driver.get('https://9gag.com/u/'+bot)
-        try:
-            if(reportAndBlock):
-                #Report User
+    buffer = []
+    posters = {}
+    lastPostTime = None
+    date = datetime.now()
+    try:
+        print("Scanning...You may now scroll to oblivion.\n")
+        print("Warning:  Scanning might produce false positives if you can't see 1h old posts at least.")
+        previous_stream = None
+        # Loop to scan continuously while on 9gag.
+        while EC.presence_of_element_located((By.CSS_SELECTOR, ".list-stream")):
+            for stream in driver.find_elements(By.CSS_SELECTOR, ".list-stream"):
+                # Prevent scaning all previous posts each frame.
+                if stream.id != previous_stream:
+                    if not stream.id in buffer:
+                        buffer.append(stream.id)
+                        previous_stream = stream.id
+                        # Scan posts in curent stream.
+                        post_list = stream.find_elements(By.CSS_SELECTOR, ".ui-post-creator")
+                        for post in post_list:
+                            try:
+                                if not post.id in buffer:
+                                    poster = post.find_element(By.CSS_SELECTOR, ".ui-post-creator__author")
+                                    time = post.find_element(By.CSS_SELECTOR, ".ui-post-creator__creation")
+                                    # print(poster.text, time.text)
+                                    lastPostTime = time.text
+                                    if not poster.text in posters.keys():
+                                        posters[poster.text] = [time.text]
+                                    else:
+                                        posters[poster.text].append(time.text)
+                                    buffer.append(post.id)
+                            except:
+                                # Promoted post or ad.
+                                pass
+    except:
+        # Estimate the scrolled time span in fresh (take the oldest post)
+        
+        if len(lastPostTime) == 9:
+            postYear = lastPostTime[-2:]
+            postMonth = monthStringToInt(lastPostTime[3:5])
+            postDay = lastPostTime[:1]
+
+            postDate = datetime.strptime(20+postYear, postMonth, postDay, '%Y %m %d')
+            scanDate = datetime.strptime(date.tm_year, date.tm_month, date.tm_day, '%Y %m %d')
+            deltaDate = scanDate - postDate
+            hoursElapsed = deltaDate.totalseconds() / 3600
+
+        elif len(lastPostTime) == 6:
+            postMonth = monthStringToInt(lastPostTime[3:5])
+            postDay = lastPostTime[:1]
+
+            postDate = datetime.strptime("2023", postMonth, postDay, '%Y %m %d')
+            scanDate = datetime.strptime("2023", date.tm_month, date.tm_day, '%Y %m %d')
+            deltaDate = scanDate - postDate
+            hoursElapsed = deltaDate.totalseconds() / 3600
+
+        elif len(lastPostTime) == 3:
+            hoursElapsed = postTimeToHour(lastPostTime[:1], lastPostTime[-1])
+        elif len(lastPostTime) == 2:
+            hoursElapsed = postTimeToHour(lastPostTime[0], lastPostTime[-1])
+        else:
+            # error in date format or "Just Now"
+            hoursElapsed = None
+
+        existingBots = []
+        bots = []       
+        with open(os.path.realpath(os.path.dirname(__file__)) + os.sep + 'bots.txt', 'r') as file:
+            for bot in file:
+                existingBots.append(bot.strip())
+
+
+        print("Elapsed time in scroll:", hoursElapsed)
+        for poster, times in posters.items():
+            if poster != "9GAGGER":
+                # Estimate the average amount of posts per hour from this user.
+                postPerHour = len(times) / hoursElapsed
+                if postPerHour > 1:
+                    if not poster in existingBots:
+                        bots.append(poster)
+
+
+        newBotsList = existingBots + bots
+        print(newBotsList)
+        newBotsList.sort()
+        print(newBotsList)
+
+        print("Here are the bots found.")
+        with open(os.path.realpath(os.path.dirname(__file__)) + os.sep + 'bots.txt', 'w') as file:
+            for bot in newBotsList:
+                file.write("\n" + bot)
+                print(bot)
+
+else:
+    driver.get("https://9gag.com/login")
+    input("Log into 9gag. Press ENTER after login.")
+
+    with open('bots.txt', 'r') as file:
+        for bot in file:
+            bot = bot.strip()
+            driver.get('https://9gag.com/u/'+bot)
+            try:
+                if(reportAndBlock):
+                    #Report User
+                    #Click Menu-Button
+                    WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".uikit-popup-menu a")))
+                    driver.find_element(By.CSS_SELECTOR, ".uikit-popup-menu a").click()
+
+                    #Click Report Button
+                    WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".uikit-popup-menu .menu ul li:nth-child(2) a")))
+                    driver.find_element(By.CSS_SELECTOR, ".uikit-popup-menu .menu ul li:nth-child(2) a").click()
+
+                    #Click Spam Button
+                    WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".report-type__type div:nth-child(2)")))
+                    driver.find_element(By.CSS_SELECTOR, ".report-type__type div:nth-child(2)").click()
+
+                    #Click Report Button
+                    WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".report-btn")))
+                    driver.find_element(By.CSS_SELECTOR, ".report-btn").click()
+
+                #Block User
                 #Click Menu-Button
                 WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".uikit-popup-menu a")))
                 driver.find_element(By.CSS_SELECTOR, ".uikit-popup-menu a").click()
 
-                #Click Report Button
-                WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".uikit-popup-menu .menu ul li:nth-child(2) a")))
-                driver.find_element(By.CSS_SELECTOR, ".uikit-popup-menu .menu ul li:nth-child(2) a").click()
+                #Find Block-Button
+                WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".uikit-popup-menu .menu ul li:nth-child(3) a")))
+                blockButton = driver.find_element(By.CSS_SELECTOR, ".uikit-popup-menu .menu ul li:nth-child(3) a")
+                print(blockButton.get_attribute('innerHTML'))
 
-                #Click Spam Button
-                WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".report-type__type div:nth-child(2)")))
-                driver.find_element(By.CSS_SELECTOR, ".report-type__type div:nth-child(2)").click()
+                #Check if User is already blocked or not
+                if "Unblock" not in blockButton.get_attribute('innerHTML'):
+                    blockButton.click()
 
-                #Click Report Button
-                WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".report-btn")))
-                driver.find_element(By.CSS_SELECTOR, ".report-btn").click()
-            
-            #Block User
-            #Click Menu-Button
-            WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".uikit-popup-menu a")))
-            driver.find_element(By.CSS_SELECTOR, ".uikit-popup-menu a").click()
-
-            #Find Block-Button
-            WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".uikit-popup-menu .menu ul li:nth-child(3) a")))
-            blockButton = driver.find_element(By.CSS_SELECTOR, ".uikit-popup-menu .menu ul li:nth-child(3) a")
-            print(blockButton.get_attribute('innerHTML'))
-
-            #Check if User is already blocked or not
-            if "Unblock" not in blockButton.get_attribute('innerHTML'):
-                blockButton.click()
-
-                WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".ui-dialog__actions button.btn-color-danger")))
-                driver.find_element(By.CSS_SELECTOR, ".ui-dialog__actions button.btn-color-danger").click()
-                print("User "+bot+" successfully blocked")
-            else:
-                driver.get("https://9gag.com")
+                    WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".ui-dialog__actions button.btn-color-danger")))
+                    driver.find_element(By.CSS_SELECTOR, ".ui-dialog__actions button.btn-color-danger").click()
+                    print("User "+bot+" successfully blocked")
+                else:
+                    driver.get("https://9gag.com")
+                    print("User "+bot+" was already blocked. Skipping...")
+            except:
                 print("User "+bot+" was already blocked. Skipping...")
-        except:
-            print("User "+bot+" was already blocked. Skipping...")
